b652c85624e84bdfeaadb92aac351c2137c30360
kuntsevich <ifk88@mail.ru>
2020-03-29 20:45:55 +0300

    clean up project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,4 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Dynamic title';
-  inputValue = '';
-
-  // img = 'https://dev-gang.ru/static/storage/24808028622541768947383165896446886988.png';
-  constructor() {
-    /*setTimeout(() => {
-      console.log('Timeout is over');
-      this.img = 'https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/21_Angular-512.png';
-    }, 5000);*/
-  }
-
-  onInput(event: KeyboardEvent) {
-    this.inputValue = (event.target as HTMLInputElement).value;
-  }
-
-  onClick() {
-    console.log('Click');
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str;
-  }
 }
